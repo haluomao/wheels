8c8dfd2754589633d090a11f8775c09d90ab80d9
maofagui <maofagui@yuewen.com>
2016-08-04 18:37:30 +0800

    回去接着弄 http://www.tutorialspoint.com/drools/drools_sample_drools_program.htm

---
+++ src/com/atongmu/sample/ItemCity.java
@@ -0,0 +1,57 @@
+package com.atongmu.sample;
+
+import java.math.BigDecimal;
+
+/**
+ * [Description]
+ * date: 2016/8/4
+ *
+ * @author maofagui
+ * @version 1.0
+ */
+public class ItemCity {
+    public enum City {
+        PUNE, NAGPUR
+    }
+
+    public enum Type {
+        GROCERIES, MEDICINES, WATCHES, LUXURYGOODS
+    }
+
+    private City purchaseCity;
+    private BigDecimal sellPrice;
+    private Type typeofItem;
+    private BigDecimal localTax;
+
+    public City getPurchaseCity() {
+        return purchaseCity;
+    }
+
+    public void setPurchaseCity(City purchaseCity) {
+        this.purchaseCity = purchaseCity;
+    }
+
+    public BigDecimal getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(BigDecimal sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public Type getTypeofItem() {
+        return typeofItem;
+    }
+
+    public void setTypeofItem(Type typeofItem) {
+        this.typeofItem = typeofItem;
+    }
+
+    public BigDecimal getLocalTax() {
+        return localTax;
+    }
+
+    public void setLocalTax(BigDecimal localTax) {
+        this.localTax = localTax;
+    }
+}
